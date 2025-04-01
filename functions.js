83d8585385bda4b73afb5501082e8f7d01e0458c
amrutya jirobe <ajirobe84@gmail.com>
2025-04-01 02:42:49 -0700

    Update functions.js
    Added Anonymous function

--- functions.js
+++ functions.js
@@ -18,3 +18,14 @@ console.log(Magic);  // displays =>
 const f = Magic;
 //executes the  function
 console.log(f()); // now we have reference to the function
+
+/**
+        ANONYMOUS FUNCTION
+ */
+//function as an object property
+const o = {
+    name: "Anonymous function",
+    invoke(){ return "You called me @.@"; }
+}
+
+console.log(o.invoke());
