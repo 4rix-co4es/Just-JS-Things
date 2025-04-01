c5dc2a2ec0534a9838a3775d05d8d6581f947c80
amrutya jirobe <ajirobe84@gmail.com>
2025-04-01 02:58:05 -0700

    Update arguments.js

--- arguments.js
+++ arguments.js
@@ -20,3 +20,30 @@ function Main(x="default"){
         }
         
         console.log(Main(10)); // 3 - since its default
+
+
+/**     DESTRUCTING ARGUMENTS */
+        
+//declare object
+    const o = {
+        w1 : "Aj",
+        w2 : "Tbj",
+        w3 : "Pkd"
+    }
+
+    function getData({w1,w2,w3}){
+        return `${w1}${w2}${w3}`;
+    }
+    console.log(getData(o));
+
+
+    const arr = ["aj","tbj","pkd"];
+
+    // specify {} square brackets when you declare destructured array
+    function getData({w1,w2,w3}){
+        return `${w1}${w2}${w3}`;
+    }
+    console.log(getData(arr));
+
+
+    
